1f97a3a74bfe68e2c79ea442eab123df5ced1447
Franklin Hau <fhauchi@gmail.com>
2023-07-17 20:51:57 -0600

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word=word
+        self.sorted_word=sorted(word.lower())
+
+    def match(self, candidates):
+        correct_anagrams = []
+
+        for candidate in candidates:
+            if sorted(candidate.lower()) == self.sorted_word and candidate.lower() != self.word.lower():
+                correct_anagrams.append(candidate)
+    
+        return correct_anagrams 
+
+anagram_finder = Anagram('listen')
+candidates = ['enlist', 'google', 'inlets', 'banana']
+results = anagram_finder.match(candidates)
+print(results)
